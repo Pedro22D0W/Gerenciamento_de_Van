923ebcb4970e44c29199d028b940148702aed1e1
tay <112909056+tay-assis@users.noreply.github.com>
2024-10-10 14:52:26 -0300

    start commit

---
+++ back-end/proj_van/src/main/java/com/example/projvan/proj_van/ProjVanApplication.java
@@ -0,0 +1,13 @@
+package com.example.projvan.proj_van;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjVanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjVanApplication.class, args);
+	}
+
+}
---
+++ back-end/proj_van/src/test/java/com/example/projvan/proj_van/ProjVanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projvan.proj_van;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjVanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
