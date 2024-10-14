73d3361e4671e6e3783bcfb6569f968415375836
Taynara Araújo de Assis <112909056+tay-assis@users.noreply.github.com>
2024-10-14 11:44:37 -0300

    Update app.component.ts

--- front-end/proj_van/src/app/app.component.ts
+++ front-end/proj_van/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
