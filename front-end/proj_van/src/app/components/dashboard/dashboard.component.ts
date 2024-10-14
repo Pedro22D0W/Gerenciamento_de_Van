c6f512c139f966995537bf8f9b802dd1bf0dfd3f
Taynara Araújo de Assis <112909056+tay-assis@users.noreply.github.com>
2024-10-14 12:01:03 -0300

    Update dashboard.component.ts

--- front-end/proj_van/src/app/components/dashboard/dashboard.component.ts
+++ front-end/proj_van/src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { Header2Component } from "../header-2/header-2.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, Header2Component],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
