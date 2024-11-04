6e6ec487b2bfff28d0d98d5ec7bd4654d0860e3e
VITOR YUZO TAKEI <vitor.yuzo.profissional@gmail.com>
2024-11-03 21:31:15 -0300

    Implementando Área Financeira do Passageiro
    Modificações:
    -Implementei +1 rota
    -Adicionei um botão de area financeira no header 2 passageiro
    -Criei um componente para area financeira do passageiro

--- front-end/proj_van/src/app/components/header-financeiro-passageiro/header-financeiro-passageiro.component.ts
+++ front-end/proj_van/src/app/components/header-financeiro-passageiro/header-financeiro-passageiro.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header-2-passageiro',
+  selector: 'app-header-financeiro-passageiro',
   standalone: true,
   imports: [],
-  templateUrl: './header-2-passageiro.component.html',
-  styleUrl: './header-2-passageiro.component.scss'
+  templateUrl: './header-financeiro-passageiro.component.html',
+  styleUrl: './header-financeiro-passageiro.component.scss'
 })
-export class Header2PassageiroComponent {
-
+export class HeaderFinanceiroPassageiroComponent {
   constructor(private router: Router) {}
 
   logOut(){
@@ -18,9 +17,4 @@ export class Header2PassageiroComponent {
     sessionStorage.clear();
     this.router.navigate(["/"]);
   }
-
-  FinanceiroPassageiro() {
-    this.router.navigate(['/financeiro-passageiro']);
-  }
-
 }
