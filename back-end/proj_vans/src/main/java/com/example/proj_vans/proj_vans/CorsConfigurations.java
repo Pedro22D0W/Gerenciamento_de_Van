2aee27b22088f19b394b5ec8308ce39f23a499a7
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2024-12-11 11:32:24 -0300

    nginx funcionando

--- back-end/proj_vans/src/main/java/com/example/proj_vans/proj_vans/CorsConfigurations.java
+++ back-end/proj_vans/src/main/java/com/example/proj_vans/proj_vans/CorsConfigurations.java
@@ -10,7 +10,7 @@ public class CorsConfigurations implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200","http://localhost:80","http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
