38a7ee2c7e8d4e60baf013a6e03dc9ce4bece5c9
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2024-12-15 13:14:58 -0300

    proxy reverso para imagens de perfil + remoção dos passageiros e motoristas

---
+++ back-end/proj_vans/src/main/resources/db/migration/V13__remove-passageiros.sql
@@ -0,0 +1,14 @@
+DELETE FROM passageiros
+WHERE email IN (
+    'joao.silva@example.com',
+    'maria.oliveira@example.com',
+    'pedro.souza@example.com',
+    'ana.santos@example.com',
+    'lucas.almeida@example.com',
+    'fernanda.costa@example.com',
+    'carlos.lima@example.com',
+    'beatriz.rocha@example.com',
+    'rafael.martins@example.com',
+    'carla.rodrigues@example.com'
+);
+
---
+++ back-end/proj_vans/src/main/resources/db/migration/V14__remove-motoristas.sql
@@ -0,0 +1,8 @@
+DELETE FROM motoristas
+WHERE email IN (
+    'carlos.silva@example.com',
+    'mariana.santos@example.com',
+    'pedro.oliveira@example.com',
+    'ana.costa@example.com',
+    'lucas.pereira@example.com'
+);
